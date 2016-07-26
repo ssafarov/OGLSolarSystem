ca26b4741136fc2a00a155e0093756df139a22ad
Safarov, Sergey <s.safarov@itransition.com>
2016-07-26 16:08:56 +0300

    Texture handling refactoring

--- globals.h
+++ globals.h
@@ -3,6 +3,10 @@
 #include <cmath>
 #include <string>
 
+#include <cstdio>
+#include <cstdlib>
+
+
 /*Published under The MIT License (MIT)
 See LICENSE.TXT*/
 
@@ -26,3 +30,5 @@ const float CMS = 0.01f;
 const float CTS = 0.05f;
 
 const float TIME_SCALE_MULTIPLIERS[17] = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.5f, 2.0f, 3.0f, 5.0f, 10.0f };
+
+
--- texture.cpp
+++ texture.cpp
@@ -3,66 +3,94 @@ See LICENSE.TXT*/
 
 // Sergey Safarov
 
-#define _CRT_SECURE_NO_DEPRECATE
-
 #include "texture.h"
 
 using namespace System::Windows::Forms;
 
-// define the header structure for
-// loading the TGA header info.
-#pragma pack(1) // pack it so that I can have shorts & chars in sequence and read directly from file memory
-struct TGAHeader {
-	char    id_length;
-	char    map_type;
-	char    type;
-	short   map_start;
-	short   map_length;
-	char    map_depth;
-	short   x_origin;
-	short   y_origin;
-	short   width;
-	short   height;
-	char    bpp;
-	char    descriptor_bits;
-};
-#pragma pack()
+
+
+bool icompare_predicate(unsigned char a, unsigned char b)
+{
+	return tolower(a) == tolower(b);
+}
+
+bool icompare(std::string const& a, std::string const& b)
+{
+	if (a.length() == b.length()) {
+		return std::equal(b.begin(), b.end(), a.begin(), icompare_predicate);
+	}
+	else {
+		return false;
+	}
+}
+
 
 Texture::Texture(char* imagePath)
 {
-	FILE *ptrFile;		// Texture file handle
+	std::string filePath = imagePath;
+	textureFileExt = filePath.substr( filePath.rfind(".") + 1 );
+	int tfeSize = textureFileExt.length();
+
+	if (tfeSize == 0)
+	{
+		MessageBox::Show("Unsupported texture file");
+		exit(1);
+	}
+	
+	// Try open the texture file
+	if (((ptrFile = fopen(imagePath, "rb")) == NULL)) {
+		MessageBox::Show("Unable to open requested texture file");
+		exit(1);
+	}
+
+	if (icompare(textureFileExt, tgaAllowed))
+	{
+		LoadTGATexture(ptrFile);
+	}
+	else if (icompare(textureFileExt, bmpAllowed))
+	{
+		LoadBMPTexture(ptrFile);
+	}
+	else
+	{
+		MessageBox::Show("Unsupported texture file");
+		exit(1);
+	}
+
+}
+
+GLuint Texture::getTextureHandle(void)
+{
+	return textureHandle;
+}
+
+void Texture::LoadTGATexture(FILE *ptrTextureFile)
+{
 	TGAHeader header;	// structure for the header info
-	char *ptrPixels, *ptrBuffer;
 
 	// column counter, row counter, i loop counters, and bytes per pixel
 	int c, r, i, j, bytesPP = 4;
 	char n, packetHeader;
 	char pixel[4];
 
-	// open the file
-	if ((ptrFile = fopen(imagePath, "rb")) == NULL) {
-		MessageBox::Show("Unable to find requested texture file");
-		exit(1);
-	}
-
 	// read the header
-	fread(&header, 18, 1, ptrFile);
-	
-	// bytes per pixel
-	bytesPP = header.bpp / 8; 
+	fread(&header, 18, 1, ptrTextureFile);
 
-	ptrPixels = (char*)malloc(bytesPP * header.width * header.height);
+	// bytes per pixel
+	bytesPP = header.bpp / 8;
+
+	ptrTexturePixels = (char*)malloc(bytesPP * header.width * header.height);
 
 	// Decode texture
 	// header type 2 is uncompressed RGB data without a color map / palette
 	if (header.type == 2)
 	{
 		// seek to the start of the data
-		fseek(ptrFile, header.map_start + header.map_length * bytesPP + 18, SEEK_SET);
+		fseek(ptrTextureFile, header.map_start + header.map_length * bytesPP + 18, SEEK_SET);
 
 		// read the pixel data into a buffer
-		ptrBuffer = (char*)malloc(bytesPP * header.width * header.height);
-		fread(ptrBuffer, bytesPP, header.width * header.height, ptrFile);
+		ptrTextureBuffer = (char*)malloc(bytesPP * header.width * header.height);
+		fread(ptrTextureBuffer, bytesPP, header.width * header.height, ptrTextureFile);
 
 		// plot the pixel data into pixels buffer
 		for (c = 0; c < header.width; c++)	// count up columns
@@ -71,27 +99,27 @@ Texture::Texture(char* imagePath)
 			{
 				if (bytesPP == 4)
 				{
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 0] = ptrBuffer[(c + r * header.width) * bytesPP + 2];
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 1] = ptrBuffer[(c + r * header.width) * bytesPP + 1];
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 2] = ptrBuffer[(c + r * header.width) * bytesPP + 0];
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 3] = ptrBuffer[(c + r * header.width) * bytesPP + 3];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 0] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 2];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 1] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 1];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 2] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 0];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 3] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 3];
 				}
 				else if (bytesPP == 3)
 				{
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 0] = ptrBuffer[(c + r * header.width) * bytesPP + 2];
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 1] = ptrBuffer[(c + r * header.width) * bytesPP + 1];
-					ptrPixels[(c + (header.height - r - 1) * header.width) * bytesPP + 2] = ptrBuffer[(c + r * header.width) * bytesPP + 0];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 0] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 2];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 1] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 1];
+					ptrTexturePixels[(c + (header.height - r - 1) * header.width) * bytesPP + 2] = ptrTextureBuffer[(c + r * header.width) * bytesPP + 0];
 				}
 			}
 		}
 
-		free(ptrBuffer);
+		free(ptrTextureBuffer);
 
 	}
 	else if (header.type == 10)	// run length encoded, non color mapped RGB
 	{
 		// find the start of the data
-		fseek(ptrFile, header.map_start + header.map_length * bytesPP + 18, SEEK_SET);
+		fseek(ptrTextureFile, header.map_start + header.map_length * bytesPP + 18, SEEK_SET);
 
 		// start at the top left
 		// work through the bitmap
@@ -100,34 +128,34 @@ Texture::Texture(char* imagePath)
 		while (r >= 0)
 		{
 			// read in the packet header
-			fread(&packetHeader, sizeof(packetHeader), 1, ptrFile);
+			fread(&packetHeader, sizeof(packetHeader), 1, ptrTextureFile);
 
 			// find the number of reps
 			n = packetHeader & 0x7F;
 
 			*((int*)pixel) = 0;
 			if (n != packetHeader) // if bit = 1, the next pixel repeated N times
-				fread(&pixel, bytesPP, 1, ptrFile);
+				fread(&pixel, bytesPP, 1, ptrTextureFile);
 
 			// loop n times
 			for (i = 0; i < n + 1; i++)
 			{
 				// if bit = 0, N individual pixels	
-				if (n == packetHeader) 
-					fread(&pixel, bytesPP, 1, ptrFile);
+				if (n == packetHeader)
+					fread(&pixel, bytesPP, 1, ptrTextureFile);
 
 				if (bytesPP == 4)
 				{
-					ptrPixels[(c + r * header.width) * bytesPP + 0] = pixel[2];
-					ptrPixels[(c + r * header.width) * bytesPP + 1] = pixel[1];
-					ptrPixels[(c + r * header.width) * bytesPP + 2] = pixel[0];
-					ptrPixels[(c + r * header.width) * bytesPP + 3] = pixel[3];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 0] = pixel[2];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 1] = pixel[1];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 2] = pixel[0];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 3] = pixel[3];
 				}
 				else if (bytesPP == 3)
 				{
-					ptrPixels[(c + r * header.width) * bytesPP + 0] = pixel[2];
-					ptrPixels[(c + r * header.width) * bytesPP + 1] = pixel[1];
-					ptrPixels[(c + r * header.width) * bytesPP + 2] = pixel[0];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 0] = pixel[2];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 1] = pixel[1];
+					ptrTexturePixels[(c + r * header.width) * bytesPP + 2] = pixel[0];
 				}
 				// move to the next pixel
 				c += 1;
@@ -140,7 +168,7 @@ Texture::Texture(char* imagePath)
 		}
 	}
 	// close the file
-	fclose(ptrFile);
+	fclose(ptrTextureFile);
 
 	// create an OpenGL texture
 	glGenTextures(1, &textureHandle);
@@ -155,13 +183,49 @@ Texture::Texture(char* imagePath)
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, header.width, header.height, GL_RGB, GL_UNSIGNED_BYTE, ptrPixels);
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, header.width, header.height, GL_RGB, GL_UNSIGNED_BYTE, ptrTexturePixels);
 
 	// delete the pixel data
-	free(ptrPixels);
+	free(ptrTexturePixels);
 }
 
-GLuint Texture::getTextureHandle(void)
+void Texture::LoadBMPTexture(FILE *ptrTextureFile)
 {
-	return textureHandle;
+	int width, height, c, whm;
+
+	width = 1024;
+	height = 512;
+	c = 3;
+	whm = width * height;
+
+	ptrTexturePixels = (char *)malloc(whm * c);
+
+	fread(ptrTexturePixels, whm * c, 1, ptrTextureFile);
+	fclose(ptrTextureFile);
+
+	for (int i = 0; i < whm; ++i)
+	{
+		int index = i * c;
+		char B, R;
+		B = ptrTexturePixels[index];
+		R = ptrTexturePixels[index + 2];
+
+		ptrTexturePixels[index] = R;
+		ptrTexturePixels[index + 2] = B;
+
+	}
+
+	glGenTextures(1, &textureHandle);
+	glBindTexture(GL_TEXTURE_2D, textureHandle);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	gluBuild2DMipmaps(GL_TEXTURE_2D, c, width, height, GL_RGB, GL_UNSIGNED_BYTE, ptrTexturePixels);
+
+	free(ptrTexturePixels);
+
 }
